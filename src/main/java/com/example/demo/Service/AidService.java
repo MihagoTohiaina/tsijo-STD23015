d1ff48ac214969fd2350c596a31adddb0119f4a3
MihagoTohiaina <mihagotohiaina@gmail.com>
2025-08-05 15:56:34 +0300

    fix:extraneous semicolon

--- src/main/java/com/example/demo/Service/AidService.java
+++ src/main/java/com/example/demo/Service/AidService.java
@@ -1,18 +1,17 @@
 package com.example.demo.Service;
 
-
 import com.example.demo.Repository.AidRepository;
 import com.example.demo.model.Aid;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AidService {
-    private final AidRepository aidRepository;
+  private final AidRepository aidRepository;
 
-    public List<Aid> getAllAids() {
-        return aidRepository.findAll();
-    }
+  public List<Aid> getAllAids() {
+    return aidRepository.findAll();
+  }
 }
